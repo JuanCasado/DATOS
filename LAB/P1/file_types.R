a880d6ab3dd42d172cd0f465783fb13a81545b6a
JuanCasado <juan.casado@edu.uah.es>
2019-09-29 17:42:05 +0200

    identify

--- LAB/P1/file_types.R
+++ LAB/P1/file_types.R
@@ -1,6 +1,6 @@
 fileTypes <- function() {
   structure(
-    list(TXT = "TXT", EXCEL = "EXCEL", SAV = "SAV", CSV = "CSV"),
+    list(TXT = "TXT", EXCEL = "EXCEL", SAV = "SAV", CSV = "CSV", DIR = "DIR", NOT_LISTED = "NOT_LISTED"),
     class= c("fileTypes", "enum", "list")
   )
 }
--- load_dependencies.R
+++ load_dependencies.R
@@ -1,3 +1,3 @@
 identify <- function(path) {
-  
+  library("tools")
 }
