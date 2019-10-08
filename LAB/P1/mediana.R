d703e4c60c3898b1f1fa7d6f7967c2702508e1ac
SamuelGarciaG <47883616+SamuelGarciaG@users.noreply.github.com>
2019-10-08 12:05:43 +0200

    Update mediana.R

--- LAB/P1/mediana.R
+++ LAB/P1/mediana.R
@@ -2,18 +2,18 @@
 
 #Calcula la mediana de un conjunto de datos
 
+#mediana <- function(data){
+#    median(x, na.rm = FALSE)
+#}
+
+
 mediana <- function(data){
-    median(x, na.rm = FALSE)
-}
-
-
-medianaPRO <- function(data){
     size = length(x[!is.na(x)])
     if (size  % 2 == 0){
-      mediana = (x[size / 2 - 1] + x[size / 2]) / 2.0
+      (x[size / 2 - 1] + x[size / 2]) / 2.0
     }
     else {
-      mediana = x[size / 2]
+      x[size / 2]
      }
 
 }
