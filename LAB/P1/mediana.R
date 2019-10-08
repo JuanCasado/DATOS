69e87625ea93052e2a22de52a7a4fabac9992f36
SamuelGarciaG <47883616+SamuelGarciaG@users.noreply.github.com>
2019-10-08 12:13:39 +0200

    Update mediana.R

--- LAB/P1/mediana.R
+++ LAB/P1/mediana.R
@@ -8,12 +8,12 @@
 
 
 mediana <- function(data){
-    size = length(x[!is.na(x)])
+    size = length(x)
     if (size  %% 2 == 0){
-      (x[size / 2 - 1] + x[size / 2]) / 2.0
+      ((x[(size / (2 - 1))] + x[size / 2]) / 2.0)
     }
     else {
-      x[size / 2]
+      x[(size / 2)]
      }
 
 }
