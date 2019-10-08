998cd40c9c667339f6eb8d9665d87fe6bff41e5d
SamuelGarciaG <47883616+SamuelGarciaG@users.noreply.github.com>
2019-10-08 21:26:57 +0200

    Mediana, cuartiles y cuantil54 funcionando

--- LAB/P1/cuartiles.R
+++ LAB/P1/cuartiles.R
@@ -3,5 +3,16 @@
 #Calcula los percentiles deseados de un conjunto de datos
 
 cuartiles <- function(data){
-    quantile(data,prob=c(.25,.5,.75), na.rm=TRUE)
+    x = sort(data)
+    size = length(x)
+    if (size  %% 2 == 0){
+        c((x[size / 4] + x[(size / 4) +1]) / 2.0 ,
+        (x[size / 2] + x[(size / 2) +1]) / 2.0 ,
+        (x[size *0.75] + x[size * 0.75 + 1]) / 2.0)
+
+    }
+    else {
+        x[size / 2]
+  }
+
 }
--- LAB/P1/mediana.R
+++ LAB/P1/mediana.R
@@ -2,18 +2,13 @@
 
 #Calcula la mediana de un conjunto de datos
 
-#mediana <- function(data){
-#    median(x, na.rm = FALSE)
-#}
-
-
-mediana <- function(x){
-    size = length(x)
-    if (size  %% 2 == 0){
-      (x[(size / 2) - 1] + x[size / 2]) / 2.0
-    }
-    else {
-      x[size / 2]
-     }
-
+mediana <- function(data){
+  x = sort(data)
+  size = length(x)
+  if (size  %% 2 == 0){
+    (x[size / 2] + x[(size / 2) +1]) / 2.0
+  }
+  else {
+     x[size / 2]
+  }
 }
