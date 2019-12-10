7c1802d57fcc8bb489a55060e4c52e175ca61f03
JuanCasado <juan.casado@edu.uah.es>
2019-12-10 19:53:57 +0100

    p6

--- LAB/_PRUEBA/file_types.R
+++
@@ -1,6 +0,0 @@
-fileTypes <- function() {
-  structure(
-    list(TXT = "TXT", EXCEL = "EXCEL", SAV = "SAV", CSV = "CSV", DIR = "DIR", NOT_LISTED = "NOT_LISTED"),
-    class= c("fileTypes", "enum", "list")
-  )
-}
--- LAB/_PRUEBA/read.R
+++
@@ -1,9 +0,0 @@
-read <- function (path, type) {
-  print(type)
-  switch(type, "TXT" = read.table(path),
-         "CSV" = read.csv(path),
-         "SAV" = read.spss(path, to.data.frame = TRUE),
-         "EXCEL" = read.xlsx(path, 1, header=TRUE, colClasses=NA),
-         "JSON" = fromJSON(file = path))
-  #fromJSON(file = path)
-}
